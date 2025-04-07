837a2b34f77d2989f86fb8853a0101ef96592730
dblackstone1 <dblackstone@orchestralabs.org>
2025-04-07 12:27:58 -0400

    chore: update airdrop array to include discord events entries

--- src/sections/MainSection/airdropList.ts
+++ src/sections/MainSection/airdropList.ts
@@ -3,7 +3,8 @@ import { AirdropEntry, VALID_AIRDROP_CATEGORIES } from './types';
 type AirdropRecipients = Record<string, AirdropEntry[]>;
 const airdropRecipients: AirdropRecipients = {};
 
-const tempAirdropArray = [
+const airdropArray = [
+  // TODO: remove first recipient here
   {
     recipient: 'symphony14jz5ppgxl0mhvpmkmtm5fr75vz24p752m5mqqe',
     entries: [
@@ -16,24 +17,92 @@ const tempAirdropArray = [
     ],
   },
   {
-    recipient: 'symphony14jz5ppgxl0mhvpmkmtm5fr75vz24p752m5mqq2',
-    entries: [
-      {
-        amount: 50,
-        denom: 'MLD',
-        reason: 'Testnet Tasks',
-      },
-      {
-        amount: 0,
-        denom: 'MLD',
-        reason: 'Fairdrop',
-      },
-      {
-        amount: 25,
-        denom: 'XYZ',
-        reason: 'Other contribution',
-      },
-    ],
+    recipient: 'Raven',
+    entries: [{ amount: 6150, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'sychonix',
+    entries: [{ amount: 3700, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'Kubbykubs',
+    entries: [{ amount: 2525, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'kimga',
+    entries: [{ amount: 2500, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'vicactive',
+    entries: [{ amount: 2500, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'dwang',
+    entries: [{ amount: 2450, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'andyflex123 VAULTD',
+    entries: [{ amount: 2450, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'kycha | Solana ID',
+    entries: [{ amount: 375, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'Bang Pateng',
+    entries: [{ amount: 275, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'N I R Z H A I CLONE',
+    entries: [{ amount: 250, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'ifan2000',
+    entries: [{ amount: 250, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'Abdul Sandana',
+    entries: [{ amount: 250, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'Rainchyy',
+    entries: [{ amount: 225, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'Axell',
+    entries: [{ amount: 25, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'taradora11',
+    entries: [{ amount: 475, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'Chun | Orochi Network',
+    entries: [{ amount: 50, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'benjamin',
+    entries: [{ amount: 25, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'ed_vndt',
+    entries: [{ amount: 0, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: '0xfearless_',
+    entries: [{ amount: 125, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'hamza2304',
+    entries: [{ amount: 150, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'saadudin06',
+    entries: [{ amount: 125, denom: 'MLD', reason: 'Discord Events' }],
+  },
+  {
+    recipient: 'alidargo08',
+    entries: [{ amount: 125, denom: 'MLD', reason: 'Discord Events' }],
   },
 ];
 
@@ -47,7 +116,7 @@ const isEntryValid = (entry: Omit<AirdropEntry, 'valid'>): boolean => {
   );
 };
 
-tempAirdropArray.forEach(({ recipient, entries }) => {
+airdropArray.forEach(({ recipient, entries }) => {
   if (!airdropRecipients[recipient]) {
     airdropRecipients[recipient] = [];
   }
